574290e659e2e9a5a6c050b7b3523910e41a93ab
adyantbalaji <hittheant@gmail.com>
2023-08-09 15:11:33 -0700

    Added working MC5

--- src/ffian/project_flow_models/__init__.py
+++ src/ffian/project_flow_models/__init__.py
@@ -3,7 +3,8 @@ from .modelMC1 import ModelMC1
 from .modelMC2 import ModelMC2
 from .modelMC3 import ModelMC3
 from .modelMC4 import ModelMC4
+from .modelMC5 import ModelMC5
 from .modelMC7 import ModelMC7
 from .solver_flow_model import Solver
 
-__all__ = ["ModelBase", "ModelMC1", "ModelMC2", "ModelMC3", "ModelMC4", "ModelMC7", "Solver"]
+__all__ = ["ModelBase", "ModelMC1", "ModelMC2", "ModelMC3", "ModelMC4", "ModelMC5", "ModelMC7", "Solver"]
